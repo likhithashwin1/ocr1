838d54461b831e026e5d6da5ac34de5fd410a8e4
likhithashwin1 <64097522+likhithashwin1@users.noreply.github.com>
2022-01-04 12:35:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img=cv2.imread('1.png')
+img=cv2.imread('1234.png')
 img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #print (pytesseract.image_to_string(ing))
 hImg,wImg,_=img.shape
